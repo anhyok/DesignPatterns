542e7c1c8b897cea2ad6c829dbbcf5ef9b3c9a76
Hyok An <anhyok@gmail.com>
2023-07-26 17:02:46 +0900

    ch01

---
+++ chap01/src/ex01/MiniDuckSumulator.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class MiniDuckSumulator {
+
+	public static void main(String[] args) {
+		Duck mallaDuck = new MallardDuck();
+		mallaDuck.performQuack();
+		mallaDuck.performFly();
+	}
+
+}
---
+++ chap01/src/ex01/Quack.java
@@ -0,0 +1,10 @@
+package ex01;
+
+public class Quack implements QuackBehavior {
+
+	@Override
+	public void quack() {
+		System.out.println("꽥");
+	}
+
+}
