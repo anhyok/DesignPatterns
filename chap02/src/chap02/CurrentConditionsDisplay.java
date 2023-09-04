4bc18da02d708d4cfdd3eb2bc050ad52f7b6a15b
Hyok An <anhyok@gmail.com>
2023-09-04 17:12:54 +0900

    .

---
+++ chap02/src/chap02/CurrentConditionsDisplay.java
@@ -0,0 +1,18 @@
+package chap02;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+
+	@Override
+	public void update(float temp, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
